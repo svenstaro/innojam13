f55111d0a4d60a86a1c22e790dcd2bfbdb013bf0
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2022-11-11 18:05:29 +0100

    Add empty systemset

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use bevy::prelude::*;
 
 fn main() {
-   App::new()
-       .add_plugins(DefaultPlugins)
-       .add_system(hello_world_system)
-       .add_system( bevy::window::close_on_esc)
-       .run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_system(hello_world_system)
+        .add_system_set(SystemSet::new())
+        .add_system(bevy::window::close_on_esc)
+        .run();
 }
 
 fn hello_world_system() {
