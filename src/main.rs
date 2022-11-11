e1b32c324bc908a13f1acfbc72c0b00ccced83a5
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2022-11-11 17:46:43 +0100

    Make the game go away

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use bevy::prelude::*;
 
 fn main() {
    App::new()
+       .add_plugins(DefaultPlugins)
        .add_system(hello_world_system)
+       .add_system( bevy::window::close_on_esc)
        .run();
 }
 
