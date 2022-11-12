4bbb56bf719356dc38cc860d909c37ba82fe030e
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2022-11-12 11:32:05 +0100

    Attempt to make enemies....

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-use bevy::{input::keyboard::KeyboardInput, prelude::*};
+use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
+use rand::Rng;
 
 #[derive(Debug, Default)]
 struct SpawnWaveEvent;
+
+#[derive(Component, Debug, Default)]
+enum EnemyType {
+    #[default]
+    Grunt,
+    Swimmer,
+    Digger,
+    Tank,
+}
+
+#[derive(Component, Debug, Default)]
+struct Enemy;
 
 fn main() {
     App::new()
@@ -15,6 +28,7 @@ fn main() {
         .add_system(print_ball_altitude)
         .add_system(shoot_water)
         .add_system(spawn_new_wave_on_event)
+        .add_system(enemy_pathfinding)
         .add_system(debug_keymap)
         .run();
 }
@@ -48,7 +62,7 @@ fn setup_physics(mut commands: Commands) {
         .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)));
 }
 
-fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
+fn print_ball_altitude(_positions: Query<&Transform, With<RigidBody>>) {
     // for transform in positions.iter() {
     //     println!("Ball altitude: {}", transform.translation.y);
     // }
@@ -86,6 +100,10 @@ fn debug_keymap(keys: Res<Input<KeyCode>>, mut spawn_wave_events: EventWriter<Sp
     }
 }
 
+fn rand_f32(l: f32, u: f32) -> f32 {
+    rand::thread_rng().gen_range(l..u)
+}
+
 fn spawn_new_wave_on_event(
     spawn_wave_events: EventReader<SpawnWaveEvent>,
     windows: Res<Windows>,
@@ -99,17 +117,42 @@ fn spawn_new_wave_on_event(
     // This prevents events staying active on the next frame.
     spawn_wave_events.clear();
 
-    let window = windows.get_primary().unwrap();
-    commands
-        .spawn()
-        .insert(RigidBody::Dynamic)
-        .insert(Collider::ball(20.0))
-        .insert(Damping {
-            linear_damping: 0.5,
-            angular_damping: 0.8,
-        })
-        .insert_bundle(TransformBundle::from(Transform::from_xyz(
-            0.0, // window.width() / 2.0,
-            0.0, 0.0,
-        )));
+    let _window = windows.get_primary().unwrap();
+
+    let wave_size = 10;
+
+    for _ in 0..wave_size {
+        let base_transform = Transform::from_xyz(0.0, 0.0, 0.0);
+        let offset = Vec3::new(rand_f32(-50.0, 50.0), rand_f32(-50.0, 50.0), 0.0);
+        let transform = base_transform.with_translation(base_transform.translation + offset);
+
+        commands
+            .spawn()
+            .insert(RigidBody::Dynamic)
+            .insert(Collider::ball(10.0))
+            .insert(Damping {
+                linear_damping: 0.90,
+                angular_damping: 0.5,
+            })
+            .insert(ExternalForce {
+                force: Vec2::new(0.0, 0.0),
+                torque: 0.0,
+            })
+            .insert_bundle(TransformBundle::from(transform))
+            .insert(Enemy)
+            .insert(EnemyType::Grunt);
+    }
+}
+
+fn enemy_pathfinding(
+    mut enemy_query: Query<(&EnemyType, &mut ExternalForce, &Transform), With<Enemy>>,
+) {
+    // TODO get target and/or map (to compute a* or something....)
+    let target = Vec2::new(500.0, 500.0);
+    for (_enemy_type, mut ext_force, transform) in enemy_query.iter_mut() {
+        let direction = target - transform.translation.truncate();
+
+        // TODO switch by enemy type
+        ext_force.force = direction.normalize() * 10.0;
+    }
 }
