6f3278cceb25bbdbde97439e48207e833a69d589
Sven-Hendrik Haase <svenstaro@gmail.com>
2022-11-12 14:16:51 +0100

    Make water look better

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 use bevy_rapier2d::prelude::*;
 use rand::Rng;
 
@@ -68,7 +68,13 @@ fn print_ball_altitude(_positions: Query<&Transform, With<RigidBody>>) {
     // }
 }
 
-fn shoot_water(buttons: Res<Input<MouseButton>>, windows: Res<Windows>, mut commands: Commands) {
+fn shoot_water(
+    buttons: Res<Input<MouseButton>>,
+    windows: Res<Windows>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut commands: Commands,
+) {
     if buttons.pressed(MouseButton::Left) {
         let window = windows.get_primary().unwrap();
 
@@ -76,7 +82,7 @@ fn shoot_water(buttons: Res<Input<MouseButton>>, windows: Res<Windows>, mut comm
             commands
                 .spawn()
                 .insert(RigidBody::Dynamic)
-                .insert(Collider::ball(5.0))
+                .insert(Collider::ball(0.5))
                 .insert(Restitution::coefficient(0.1))
                 .insert(Damping {
                     linear_damping: 1.0,
@@ -86,11 +92,17 @@ fn shoot_water(buttons: Res<Input<MouseButton>>, windows: Res<Windows>, mut comm
                     impulse: Vec2::new(5.0, -5.0),
                     torque_impulse: 0.0,
                 })
-                .insert_bundle(TransformBundle::from(Transform::from_xyz(
-                    position.x - window.width() / 2.0,
-                    position.y - window.height() / 2.0,
-                    0.0,
-                )));
+                .insert_bundle(MaterialMesh2dBundle {
+                    mesh: meshes.add(Mesh::from(shape::Circle::default())).into(),
+                    transform: Transform::from_xyz(
+                        position.x - window.width() / 2.0,
+                        position.y - window.height() / 2.0,
+                        0.0,
+                    )
+                    .with_scale(Vec3::splat(10.)),
+                    material: materials.add(ColorMaterial::from(Color::BLUE)),
+                    ..default()
+                });
         } else {
             // cursor is not inside the window
         }
